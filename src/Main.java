43c873f8ed300437e1b6f796d355d777d4d23575
Sergio Rossi <sarossi@gmail.com>
2025-05-15 13:46:50 -0400

    SegundoCommit

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.google.gson.JsonObject;
+
 public class Main {
     public static void main(String[] args) {
 
-        public static void main(String[] args){
+
             System.out.printf("Hello and welcome!");
             CurrencyConverter converter = new CurrencyConverter();
             JsonObject data = converter.getCurrencyData("USD");
             if (data != null) {
                 System.out.println(data.toString());
             }
-        }
+
 
     }
 }
